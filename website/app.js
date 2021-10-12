e71f991ee9d03e8ed48b96e58d5675c105bf186f
shahinda <shahinda@pharous.com>
2021-10-12 22:31:09 +0200

    update

--- website/app.js
+++ website/app.js
@@ -2,11 +2,11 @@
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
 let feelings =  document.getElementById('feelings').value;
 // Personal API Key for OpenWeatherMap API
 let baseURL = 'https://api.openweathermap.org/data/2.5/weather?'
-let apiKey = '&appid=38944314f1a55293bf5e2a6324759cde';
+const apiKey = '&appid=38944314f1a55293bf5e2a6324759cde&units=metric';
 
 // Event listener to add function to existing HTML DOM element
 document.getElementById('generate').addEventListener('click', performAction);
@@ -20,11 +20,12 @@ function performAction(e){
     .then(function(data){
       // Add data
       console.log(data);
-      postData('/Weatherdata', {date:newDate, temp:data.main.temp, content:data.weather[0].main, feeling: feelings} );
+      postData('/Weatherdata', {date:newDate, temp:data.main.temp, content:data.weather[0].main, feeling: feelings} )
+
     })
-    .then(
-      updateUI()
-    )
+    .then(()=>
+    updateUI()
+  );
 
     }
 /* Function to GET Web API Data*/
